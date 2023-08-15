730032ab98e2d8ab8390a06d570afea639039bba
王良 <841369634@qq.com>
2023-08-15 18:09:55 +0800

    Revert "小调整。"
    This reverts commit 90b894a33a36037b1abf39135b0f28c0cdffb08b.

--- rm-datasource/src/main/java/io/seata/rm/datasource/exec/UpdateExecutor.java
+++ rm-datasource/src/main/java/io/seata/rm/datasource/exec/UpdateExecutor.java
@@ -134,9 +134,8 @@ public class UpdateExecutor<T, S extends Statement> extends AbstractDMLBaseExecu
                     needUpdateColumns.add(getColumnNamesWithTablePrefix(table,tableAlias,pkNameList));
                 }
             }
-            List<String> columns = unescapeUpdateColumns.stream()
-                    .map(unescapeUpdateColumn -> ColumnUtils.addEscape(unescapeUpdateColumn, getDbType(), tableMeta)).collect(Collectors.toList());
-            needUpdateColumns.addAll(columns);
+            needUpdateColumns.addAll(unescapeUpdateColumns.stream()
+                .map(unescapeUpdateColumn -> ColumnUtils.addEscape(unescapeUpdateColumn, getDbType(), tableMeta)).collect(Collectors.toList()));
 
             // The on update xxx columns will be auto update by db, so it's also the actually updated columns
             List<String> onUpdateColumns = tableMeta.getOnUpdateColumnsOnlyName();
