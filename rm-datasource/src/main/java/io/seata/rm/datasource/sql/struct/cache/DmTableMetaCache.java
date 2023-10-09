5eeb3df02eaaf29cdf17eef3c86d41cc9306e046
王良 <841369634@qq.com>
2023-10-09 17:22:30 +0800

    fix merge

--- rm-datasource/src/main/java/io/seata/rm/datasource/sql/struct/cache/DmTableMetaCache.java
+++ rm-datasource/src/main/java/io/seata/rm/datasource/sql/struct/cache/DmTableMetaCache.java
@@ -24,7 +24,6 @@ import io.seata.sqlparser.struct.IndexType;
 import io.seata.sqlparser.struct.TableMeta;
 import io.seata.sqlparser.util.JdbcConstants;
 
-import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -55,16 +54,6 @@ public class DmTableMetaCache extends OracleTableMetaCache {
     }
 
     @Override
-    protected TableMeta fetchSchema(Connection connection, String tableName) throws SQLException {
-        try {
-            return resultSetMetaToSchema(connection.getMetaData(), tableName);
-        } catch (SQLException sqlEx) {
-            throw sqlEx;
-        } catch (Exception e) {
-            throw new SQLException(String.format("Failed to fetch schema of %s", tableName), e);
-        }
-    }
-
     protected TableMeta resultSetMetaToSchema(DatabaseMetaData dbmd, String tableName) throws SQLException {
         TableMeta result = new TableMeta();
         result.setTableName(tableName);
